749756459afc048a037e20364fb2a702b42452b8
James Hudson <jwhudson6@gmail.com>
2020-01-17 14:20:31 +0000

    Complete RPS Bonus Features + Rubocop

--- Lesson_2/rock_paper_scissors.rb
+++ Lesson_2/rock_paper_scissors.rb
@@ -1,31 +1,41 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+abbreviated_choices = %w(ro pa sc li sp)
+
+winning_combination = { "rock" => ["lizard", "scissors"],
+                        "paper" => ["rock", "spock"],
+                        "scissors" => ["paper", "lizard"],
+                        "lizard" => ["spock", "paper"],
+                        "spock" => ["rock", "scissors"] }
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def win?(first, second)
-  (first == "rock" && second == "scissors") ||
-    (first == "paper" && second == "rock") ||
-    (first == "scissors" && second == "paper")
+def win?(first, second, combo)
+  combo[first].include?(second)
 end
 
-def display_results(player, computer)
-  if win?(player, computer)
+def display_results(player, computer, combo)
+  if win?(player, computer, combo)
     prompt("You Won!")
-  elsif win?(computer, player)
-    prompt("Computer Won!!")
+  elsif win?(computer, player, combo)
+    prompt("Computer Won!")
   else
     prompt("It's a tie")
   end
 end
 
+player_score = 0
+computer_score = 0
+
 loop do
   choice = ""
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
-
+    choice = gets.chomp.downcase
+    if abbreviated_choices.include?(choice)
+      choice = VALID_CHOICES.select { |move| move.match(choice) }[0]
+    end
     if VALID_CHOICES.include?(choice)
       break
     else
@@ -37,7 +47,25 @@ loop do
 
   puts "You chose: #{choice}; Computer chose: #{computer_choice}"
 
-  display_results(choice, computer_choice)
+  display_results(choice, computer_choice, winning_combination)
+
+  if win?(choice, computer_choice, winning_combination)
+    player_score += 1
+  end
+
+  if win?(computer_choice, choice, winning_combination)
+    computer_score += 1
+  end
+
+  if computer_score == 5
+    puts "The computer wins! #{computer_score} - #{player_score}"
+    break
+  end
+
+  if player_score == 5
+    puts "You win! #{player_score} - #{computer_score}"
+    break
+  end
 
   prompt("Do you want to play again?")
   answer = gets.chomp
